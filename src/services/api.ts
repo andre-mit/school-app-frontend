5c94cb85e1556d75ef55c6be54c6c490c24843b0
andre-mit <andre.augusto222@gmail.com>
2020-12-02 00:39:02 -0300

    api public url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.API_BASE_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
